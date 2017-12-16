43e3393a3eb95e0e9b5c538892f098da9615825d
TheGame Nicorus <nicorus_1@hotmail.com>
2017-12-16 07:12:25 +0700

    add label to error message

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-
-import defaultRules from './defaultRules';
-import defaultMessages from './defaultMessages';
+import defaultRules from "./defaultRules";
+import defaultMessages from "./defaultMessages";
 
 export default class ValidationComponent extends Component {
-
   constructor(props) {
-      super(props);
-      // array to store error on each fields
-      // ex:
-      // [{ fieldName: "name", messages: ["The field name is required."] }]
-      this.errors = [];
-      // Retrieve props
-      this.deviceLocale = props.deviceLocale || 'en'; // ex: en, fr
-      this.rules = props.rules || defaultRules; // rules for Validation
-      this.messages = props.messages || defaultMessages;
+    super(props);
+    // array to store error on each fields
+    // ex:
+    // [{ fieldName: "name", messages: ["The field name is required."] }]
+    this.errors = [];
+    // Retrieve props
+    this.deviceLocale = props.deviceLocale || "en"; // ex: en, fr
+    this.rules = props.rules || defaultRules; // rules for Validation
+    this.messages = props.messages || defaultMessages;
   }
 
   /*
@@ -43,17 +41,26 @@ export default class ValidationComponent extends Component {
         // Check rule for current field
         this._checkRules(key, rules, this.state[key]);
       }
-    };
+    }
     return this.isFormValid();
   }
 
   // Method to check rules on a spefific field
   _checkRules(fieldName, rules, value) {
     for (const key of Object.keys(rules)) {
-      const isRuleFn = (typeof this.rules[key] == "function");
-      const isRegExp = (this.rules[key] instanceof RegExp);
-      if ((isRuleFn && !this.rules[key](rules[key], value)) || (isRegExp && !this.rules[key].test(value))) {
-        this._addError(fieldName, key, rules[key], isRuleFn);
+      const isRuleFn = typeof this.rules[key] == "function";
+      const isRegExp = this.rules[key] instanceof RegExp;
+      if (
+        (isRuleFn && !this.rules[key](rules[key], value)) ||
+        (isRegExp && !this.rules[key].test(value))
+      ) {
+        this._addError(
+          fieldName,
+          value.label ? value.label : fieldName,
+          key,
+          rules[key],
+          isRuleFn
+        );
       }
     }
   }
@@ -61,8 +68,10 @@ export default class ValidationComponent extends Component {
   // Add error
   // ex:
   // [{ fieldName: "name", messages: ["The field name is required."] }]
-  _addError(fieldName, rule, value, isFn) {
-    const errMsg = this.messages[this.deviceLocale][rule].replace("{0}", fieldName).replace("{1}", value);
+  _addError(fieldName, label, rule, value, isFn) {
+    const errMsg = this.messages[this.deviceLocale][rule]
+      .replace("{0}", label)
+      .replace("{1}", value);
     let [error] = this.errors.filter(err => err.fieldName === fieldName);
     // error already exists
     if (error) {
@@ -86,7 +95,7 @@ export default class ValidationComponent extends Component {
 
   // Method to check if the field is in error
   isFieldInError(fieldName) {
-    return (this.errors.filter(err => err.fieldName === fieldName).length > 0);
+    return this.errors.filter(err => err.fieldName === fieldName).length > 0;
   }
 
   isFormValid() {
@@ -94,17 +103,17 @@ export default class ValidationComponent extends Component {
   }
 
   // Concatenate each error messages
-  getErrorMessages(separator="\n") {
-    return this.errors.map((err) => err.messages.join(separator)).join(separator);
+  getErrorMessages(separator = "\n") {
+    return this.errors.map(err => err.messages.join(separator)).join(separator);
   }
 
   // Method to return errors on a specific field
   getErrorsInField(fieldName) {
-    const foundError = this.errors.find(err => err.fieldName === fieldName)
+    const foundError = this.errors.find(err => err.fieldName === fieldName);
     if (!foundError) {
-      return []
+      return [];
     }
-    return foundError.messages
+    return foundError.messages;
   }
 }
 
@@ -112,5 +121,5 @@ export default class ValidationComponent extends Component {
 ValidationComponent.propTypes = {
   deviceLocale: PropTypes.string, // Used for language locale
   rules: PropTypes.object, // rules for validations
-  messages : PropTypes.object // messages for validation errors
-}
+  messages: PropTypes.object // messages for validation errors
+};
